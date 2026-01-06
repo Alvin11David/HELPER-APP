ef79cb229e5d4ea0ec5024fc4c46d74af5ae9be5
Alvin11David <alvin69david@gmail.com>
2026-01-07 00:44:37 +0300

    Changed the asset location

--- helper/lib/Intro/Splash_Screen.dart
+++ helper/lib/Intro/Splash_Screen.dart
@@ -5,12 +5,35 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/background/normalscreen.png'),
             fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icons/logo.png',
+                width: screenWidth * 0.15,
+                height: screenWidth * 0.15,
+              ),
+              SizedBox(width: screenWidth * 0.05),
+              const Text(
+                'Helper',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ),
       ),
