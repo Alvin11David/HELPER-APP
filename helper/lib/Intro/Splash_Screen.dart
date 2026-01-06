e1156b8154f7fcec65be8cd07a12c74eeaa433ac
Alvin11David <alvin69david@gmail.com>
2026-01-07 00:46:38 +0300

    Added the ocons folder

--- helper/lib/Intro/Splash_Screen.dart
+++ helper/lib/Intro/Splash_Screen.dart
@@ -16,24 +16,26 @@ class SplashScreen extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/icons/logo.png',
-                width: screenWidth * 0.15,
-                height: screenWidth * 0.15,
-              ),
-              SizedBox(width: screenWidth * 0.05),
-              const Text(
-                'Helper',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+          child: ClipRect(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/icons/logo.png',
+                  width: screenWidth * 0.12,
+                  height: screenWidth * 0.12,
                 ),
-              ),
-            ],
+                SizedBox(width: screenWidth * 0.04),
+                Text(
+                  'Helper',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenWidth * 0.07,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
