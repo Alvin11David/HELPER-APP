5a1332744a84cac36d67fe9b780bc54a0311b65f
FidelJones <leftoversme@gmail.com>
2026-01-07 09:20:19 +0300

    done with the first three

--- helper/lib/main.dart
+++ helper/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:helper/Auth/OTP_Verification_Screen.dart';
 import 'package:helper/intro/Role_Selection_Screen.dart';
 import 'package:helper/intro/Splash_Screen.dart';
 import 'package:helper/Auth/Phone_Number_&_Email_Address_Screen.dart';
-
+import 'package:helper/Auth/Forgot_Password_Screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,8 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-      home: const PhoneNumberEmailAddressScreen(),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const OTPVerificationScreen(),
     );
   }
 }
@@ -108,4 +108,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
