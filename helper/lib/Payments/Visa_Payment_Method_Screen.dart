f82044af501dfcd453bd37975562dc18cac14582
Alvin11David <alvin69david@gmail.com>
2026-01-08 07:51:35 +0300

    Added the The chevron icon

--- helper/lib/Payments/Visa_Payment_Method_Screen.dart
+++ helper/lib/Payments/Visa_Payment_Method_Screen.dart
@@ -5,8 +5,8 @@ class VisaPaymentMethodScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double w = MediaQuery.of(context).size.width;
-    final double h = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -19,25 +19,40 @@ class VisaPaymentMethodScreen extends StatelessWidget {
           child: Stack(
             children: [
               Positioned(
-                top: h * 0.04,
-                left: w * 0.04,
-                child: GestureDetector(
-                  onTap: () => Navigator.of(context).maybePop(),
-                  child: Container(
-                    width: w * 0.13,
-                    height: w * 0.13,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFFFFFFF),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Center(
-                      child: Icon(
-                        Icons.chevron_left,
-                        color: Colors.black,
-                        size: w * 0.10,
+                top: screenHeight * 0.04,
+                left: screenWidth * 0.04,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    GestureDetector(
+                      onTap: () => Navigator.of(context).maybePop(),
+                      child: Container(
+                        width: screenWidth * 0.13,
+                        height: screenWidth * 0.13,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFFFFFFF),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.chevron_left,
+                            color: Colors.black,
+                            size: screenWidth * 0.10,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    SizedBox(width: screenWidth * 0.06),
+                    Text(
+                      'Payment Method',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.08,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
