f72bc6056c65233cae63cdef163100ba27a7972a
Alvin11David <alvin69david@gmail.com>
2026-01-08 07:49:26 +0300

    Added the The chevron icon

--- helper/lib/Payments/Visa_Payment_Method_Screen.dart
+++ helper/lib/Payments/Visa_Payment_Method_Screen.dart
@@ -5,6 +5,8 @@ class VisaPaymentMethodScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double w = MediaQuery.of(context).size.width;
+    final double h = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -13,6 +15,32 @@ class VisaPaymentMethodScreen extends StatelessWidget {
               image: AssetImage('assets/background/normalscreenbg.png'),
               fit: BoxFit.cover,
             ),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: h * 0.04,
+                left: w * 0.04,
+                child: GestureDetector(
+                  onTap: () => Navigator.of(context).maybePop(),
+                  child: Container(
+                    width: w * 0.13,
+                    height: w * 0.13,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFFFFFF),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.chevron_left,
+                        color: Colors.black,
+                        size: w * 0.10,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
