1dc7716ca18bc12867667ee0e8e5e54727eddd43
Alvin11David <alvin69david@gmail.com>
2026-01-10 11:33:48 +0300

    Working on the front scan screen

--- helper/lib/Document Upload/National_ID_Passport_Front_Scan_Screen.dart
+++ helper/lib/Document Upload/National_ID_Passport_Front_Scan_Screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class NationalIdPassportFrontScanScreen extends StatelessWidget {
@@ -50,6 +52,62 @@ class NationalIdPassportFrontScanScreen extends StatelessWidget {
                 ],
               ),
             ),
+            Positioned(
+                top:
+                    screenHeight * 0.14, // Adjusted to reduce space from header
+                left: screenWidth * 0.10,
+                right: screenWidth * 0.10,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.02,
+                        vertical: screenHeight * 0.004,
+                      ),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.white.withOpacity(0.25),
+                            Colors.white.withOpacity(0.15),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(
+                          color: Colors.white.withOpacity(0.4),
+                          width: 2,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.1),
+                            blurRadius: 15,
+                            spreadRadius: 2,
+                          ),
+                        ],
+                      ),
+                      height: 33,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Place the front part of your ID in the frame',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: screenWidth * 0.034,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Poppins',
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
           ],
         ),
       ),
