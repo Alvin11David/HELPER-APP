1cfed5c44eaa55b7e998b675a75c60cdcb0e6c08
Alvin11David <alvin69david@gmail.com>
2026-01-10 11:28:52 +0300

    Working on the front scan screen

--- helper/lib/Document Upload/National_ID_Passport_Front_Scan_Screen.dart
+++ helper/lib/Document Upload/National_ID_Passport_Front_Scan_Screen.dart
@@ -5,6 +5,54 @@ class NationalIdPassportFrontScanScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black, body: Container());
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: screenHeight * 0.04,
+              left: screenWidth * 0.04,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).maybePop(),
+                    child: Container(
+                      width: screenWidth * 0.13,
+                      height: screenWidth * 0.13,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFFFFFFF),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.chevron_left,
+                          color: Colors.black,
+                          size: screenWidth * 0.10,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: screenWidth * 0.06),
+                  Text(
+                    'ID Front',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.06,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
