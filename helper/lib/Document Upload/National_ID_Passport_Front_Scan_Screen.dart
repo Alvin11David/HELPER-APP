692290feb07b2d396eda8cd2c988cd02fa180ee4
Alvin11David <alvin69david@gmail.com>
2026-01-10 11:48:08 +0300

    Added logic for opening the camera

--- helper/lib/Document Upload/National_ID_Passport_Front_Scan_Screen.dart
+++ helper/lib/Document Upload/National_ID_Passport_Front_Scan_Screen.dart
@@ -26,10 +26,7 @@ class _NationalIdPassportFrontScanScreenState
     final cameras = await availableCameras();
     final firstCamera = cameras.first;
 
-    _controller = CameraController(
-      firstCamera,
-      ResolutionPreset.high,
-    );
+    _controller = CameraController(firstCamera, ResolutionPreset.high);
 
     _initializeControllerFuture = _controller.initialize();
   }
@@ -62,175 +59,196 @@ class _NationalIdPassportFrontScanScreenState
                   ),
                   child: BackdropFilter(
                     filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                    child: Container(
-                      color: Colors.black.withOpacity(0.5),
-                    ),
+                    child: Container(color: Colors.black.withOpacity(0.5)),
                   ),
                 ),
-            Positioned(
-              top: screenHeight * 0.04,
-              left: screenWidth * 0.04,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  GestureDetector(
-                    onTap: () => Navigator.of(context).maybePop(),
-                    child: Container(
-                      width: screenWidth * 0.13,
-                      height: screenWidth * 0.13,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFFFFFFF),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.chevron_left,
-                          color: Colors.black,
-                          size: screenWidth * 0.10,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: screenWidth * 0.06),
-                  Text(
-                    'ID Front',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: screenWidth * 0.06,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat',
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: screenHeight * 0.14, // Adjusted to reduce space from header
-              left: screenWidth * 0.10,
-              right: screenWidth * 0.10,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: screenWidth * 0.02,
-                      vertical: screenHeight * 0.004,
-                    ),
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          Colors.white.withOpacity(0.25),
-                          Colors.white.withOpacity(0.15),
-                        ],
-                      ),
-                      borderRadius: BorderRadius.circular(30),
-                      border: Border.all(
-                        color: Colors.white.withOpacity(0.4),
-                        width: 2,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.white.withOpacity(0.1),
-                          blurRadius: 15,
-                          spreadRadius: 2,
-                        ),
-                      ],
-                    ),
-                    height: 33,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Place the front part of your ID in the frame',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: screenWidth * 0.034,
-                            fontWeight: FontWeight.w500,
-                            fontFamily: 'Poppins',
+                Positioned(
+                  top: screenHeight * 0.04,
+                  left: screenWidth * 0.04,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.of(context).maybePop(),
+                        child: Container(
+                          width: screenWidth * 0.13,
+                          height: screenWidth * 0.13,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFFFFFFF),
+                            borderRadius: BorderRadius.circular(15),
                           ),
-                          textAlign: TextAlign.center,
+                          child: Center(
+                            child: Icon(
+                              Icons.chevron_left,
+                              color: Colors.black,
+                              size: screenWidth * 0.10,
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(width: screenWidth * 0.06),
+                      Text(
+                        'ID Front',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: screenWidth * 0.06,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ),
-            Positioned(
-              left: (screenWidth - 296) / 2,
-              top: (screenHeight - 489) / 2 + 20,
-              child: Container(
-                width: 296,
-                height: 489,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white, width: 2),
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: screenHeight * 0.03,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: screenWidth * 0.06,
-                        vertical: screenHeight * 0.011,
-                      ),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                            Colors.white.withOpacity(0.25),
-                            Colors.white.withOpacity(0.15),
+                Positioned(
+                  top:
+                      screenHeight *
+                      0.14, // Adjusted to reduce space from header
+                  left: screenWidth * 0.10,
+                  right: screenWidth * 0.10,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: screenWidth * 0.02,
+                          vertical: screenHeight * 0.004,
+                        ),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Colors.white.withOpacity(0.25),
+                              Colors.white.withOpacity(0.15),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(
+                            color: Colors.white.withOpacity(0.4),
+                            width: 2,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white.withOpacity(0.1),
+                              blurRadius: 15,
+                              spreadRadius: 2,
+                            ),
                           ],
                         ),
-                        borderRadius: BorderRadius.circular(30),
-                        border: Border.all(
-                          color: Colors.white.withOpacity(0.4),
-                          width: 2,
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.white.withOpacity(0.1),
-                            blurRadius: 15,
-                            spreadRadius: 2,
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SizedBox(width: screenWidth * 0.02),
-                          Text(
-                            'Make sure text is readable before submitting',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: screenWidth * 0.03,
-                              fontWeight: FontWeight.w500,
-                              fontFamily: 'Poppins',
+                        height: 33,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Place the front part of your ID in the frame',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: screenWidth * 0.034,
+                                fontWeight: FontWeight.w500,
+                                fontFamily: 'Poppins',
+                              ),
+                              textAlign: TextAlign.center,
                             ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          ],
-        ),
+                Positioned(
+                  left: (screenWidth - 296) / 2,
+                  top: (screenHeight - 489) / 2 + 20,
+                  child: Container(
+                    width: 296,
+                    height: 489,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: screenHeight * 0.03,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: screenWidth * 0.06,
+                            vertical: screenHeight * 0.011,
+                          ),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Colors.white.withOpacity(0.25),
+                                Colors.white.withOpacity(0.15),
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(
+                              color: Colors.white.withOpacity(0.4),
+                              width: 2,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white.withOpacity(0.1),
+                                blurRadius: 15,
+                                spreadRadius: 2,
+                              ),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              SizedBox(width: screenWidth * 0.02),
+                              Text(
+                                'Make sure text is readable before submitting',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: screenWidth * 0.03,
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Poppins',
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
+}
+
+class InvertedRectangleClipper extends CustomClipper<Path> {
+  final Rect rect;
+
+  InvertedRectangleClipper({required this.rect});
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path()..addRect(Rect.fromLTWH(0, 0, size.width, size.height));
+    path.addRRect(RRect.fromRectAndRadius(rect, const Radius.circular(30)));
+    path.fillType = PathFillType.evenOdd;
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
