d050db49cd247572525df4aadf1d946251499f0d
Alvin11David <alvin69david@gmail.com>
2026-01-11 22:10:19 +0300

    Working on the bottom navbar

--- helper/lib/Components/Bottom_Nav_Bar.dart
+++ helper/lib/Components/Bottom_Nav_Bar.dart
@@ -4,15 +4,47 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
+      height: 77,
       width: MediaQuery.of(context).size.width,
       color: Color(0xFFFFCB05),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Icon(Icons.home, color: Colors.black),
-          Icon(Icons.search, color: Colors.black),
-          Icon(Icons.person, color: Colors.black),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.home, color: Colors.black),
+              Text('Home', style: TextStyle(color: Colors.black)),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.map, color: Colors.black),
+              Text('Map', style: TextStyle(color: Colors.black)),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.account_balance_wallet, color: Colors.black),
+              Text('Wallet', style: TextStyle(color: Colors.black)),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.chat, color: Colors.black),
+              Text('Chat', style: TextStyle(color: Colors.black)),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.person, color: Colors.black),
+              Text('Profile', style: TextStyle(color: Colors.black)),
+            ],
+          ),
         ],
       ),
     );
