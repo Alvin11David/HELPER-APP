2cf1f9fae1bdc227b70fb740098e604469dec762
Alvin11David <alvin69david@gmail.com>
2026-01-13 15:10:32 +0300

    Working on the Wallet Topup screen

--- helper/lib/Wallet/Wallet_TopUp_Screen.dart
+++ helper/lib/Wallet/Wallet_TopUp_Screen.dart
@@ -20,14 +20,11 @@ class _WalletTopUpScreenState extends State<WalletTopUpScreen> {
 
   Widget _amountButton(double w, double h, String text) {
     return Container(
-      width: w * 0.15,
+      width: w * 0.18,
       height: h * 0.04,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        border: Border.all(
-          color: const Color(0xFFFFA10D),
-          width: 2,
-        ),
+        border: Border.all(color: const Color(0xFFFFA10D), width: 2),
       ),
       child: Center(
         child: Text(
@@ -254,11 +251,31 @@ class _WalletTopUpScreenState extends State<WalletTopUpScreen> {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
-                                _amountButton(screenWidth, screenHeight, "UGX 10,000"),
-                                _amountButton(screenWidth, screenHeight, "UGX 25,000"),
-                                _amountButton(screenWidth, screenHeight, "UGX 50,000"),
-                                _amountButton(screenWidth, screenHeight, "UGX 100,000"),
-                                _amountButton(screenWidth, screenHeight, "UGX 200,000"),
+                                _amountButton(
+                                  screenWidth,
+                                  screenHeight,
+                                  "UGX 10,000",
+                                ),
+                                _amountButton(
+                                  screenWidth,
+                                  screenHeight,
+                                  "UGX 25,000",
+                                ),
+                                _amountButton(
+                                  screenWidth,
+                                  screenHeight,
+                                  "UGX 50,000",
+                                ),
+                                _amountButton(
+                                  screenWidth,
+                                  screenHeight,
+                                  "UGX 100,000",
+                                ),
+                                _amountButton(
+                                  screenWidth,
+                                  screenHeight,
+                                  "UGX 200,000",
+                                ),
                               ],
                             ),
                           ],
