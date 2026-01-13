cfcc08ac3897bfda58bb0262a6ebc715dba7306d
Alvin11David <alvin69david@gmail.com>
2026-01-13 14:24:51 +0300

    Working on the Wallet Topup screen

---
+++ helper/lib/Wallet/Wallet_TopUp_Screen.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter/material.dart';
+
+class WalletTopUpScreen extends StatelessWidget {
+  const WalletTopUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background/normalscreenbg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const Center(child: Text('Wallet Top Up Screen')),
+      ),
+    );
+  }
+}
