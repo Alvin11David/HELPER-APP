6e362259ccad4f267e5d5c81234563153b278454
Alvin11David <alvin69david@gmail.com>
2026-01-13 14:42:27 +0300

    Working on the Wallet Topup screen

--- helper/lib/Wallet/Wallet_TopUp_Screen.dart
+++ helper/lib/Wallet/Wallet_TopUp_Screen.dart
@@ -204,3 +204,28 @@ class WalletTopUpScreen extends StatelessWidget {
     );
   }
 }
+
+// Dashed line painter
+class _DashedLinePainter extends CustomPainter {
+  final Color color;
+  _DashedLinePainter({required this.color});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..strokeWidth = 1.2;
+
+    const dashWidth = 6.0;
+    const dashSpace = 5.0;
+    double startX = 0;
+
+    while (startX < size.width) {
+      canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), paint);
+      startX += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant _DashedLinePainter oldDelegate) => false;
+}
