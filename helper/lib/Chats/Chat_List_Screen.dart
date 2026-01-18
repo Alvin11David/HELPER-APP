1cbfde88d26eeeb442090b30ac86c9c3b569adab
Alvin11David <alvin69david@gmail.com>
2026-01-18 11:30:33 +0300

    Working on the chat list screen

--- helper/lib/Chats/Chat_List_Screen.dart
+++ helper/lib/Chats/Chat_List_Screen.dart
@@ -4,6 +4,7 @@ class ChatListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Stack(
@@ -47,15 +48,21 @@ class ChatListScreen extends StatelessWidget {
               ),
             ),
             Positioned(
-              top: screenWidth * 0.05,
+              top: screenHeight * 0.03,
               left: screenWidth * 0.04,
-              child: Text(
-                'Messages',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: screenWidth * 0.06,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Messages',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.07,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat', 
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
