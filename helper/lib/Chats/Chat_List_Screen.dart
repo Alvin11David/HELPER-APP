bf1c80fd04d3d6913528cd4a6ddd62c3932b05d9
Alvin11David <alvin69david@gmail.com>
2026-01-18 13:28:50 +0300

    Working on the chat list screen

--- helper/lib/Chats/Chat_List_Screen.dart
+++ helper/lib/Chats/Chat_List_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Components/Bottom_Nav_Bar.dart';
 
 class ChatListScreen extends StatefulWidget {
   @override
@@ -8,6 +9,7 @@ class ChatListScreen extends StatefulWidget {
 class _ChatListScreenState extends State<ChatListScreen> {
   final ScrollController _scrollController = ScrollController();
   double _scale = 1.0;
+  bool hasMessages = true; // Set to true when there are actual messages
 
   @override
   void initState() {
@@ -19,6 +21,11 @@ class _ChatListScreenState extends State<ChatListScreen> {
   void dispose() {
     _scrollController.dispose();
     super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    // Add navigation logic here based on the tapped index
+    print('Bottom nav item tapped: $index');
   }
 
   void _onScroll() {
@@ -147,122 +154,156 @@ class _ChatListScreenState extends State<ChatListScreen> {
                       ),
                     ),
                     SizedBox(height: 20),
-                    SizedBox(
-                      height: screenWidth * 0.2,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: List.generate(
-                          9,
-                          (index) => Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 9.0,
-                            ),
-                            child: Column(
-                              children: [
-                                Stack(
-                                  children: [
-                                    Container(
-                                      width: screenWidth * 0.12,
-                                      height: screenWidth * 0.12,
-                                      decoration: const BoxDecoration(
-                                        color: Colors.white,
-                                        shape: BoxShape.circle,
-                                      ),
-                                    ),
-                                    Positioned(
-                                      bottom: 0,
-                                      right: 0,
-                                      child: Container(
-                                        width: 10,
-                                        height: 10,
+                    if (hasMessages)
+                      SizedBox(
+                        height: screenWidth * 0.2,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: List.generate(
+                            9,
+                            (index) => Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 9.0,
+                              ),
+                              child: Column(
+                                children: [
+                                  Stack(
+                                    children: [
+                                      Container(
+                                        width: screenWidth * 0.12,
+                                        height: screenWidth * 0.12,
                                         decoration: const BoxDecoration(
-                                          color: Color.fromARGB(255, 0, 233, 8),
+                                          color: Colors.white,
                                           shape: BoxShape.circle,
                                         ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 8),
-                                Text(
-                                  'Names',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: screenWidth * 0.03,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Column(
-                      children: List.generate(
-                        20,
-                        (index) => Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Row(
-                            children: [
-                              Stack(
-                                children: [
-                                  Container(
-                                    width: 40,
-                                    height: 40,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      shape: BoxShape.circle,
-                                    ),
-                                  ),
-                                  Positioned(
-                                    bottom: 0,
-                                    right: 0,
-                                    child: Container(
-                                      width: 10,
-                                      height: 10,
-                                      decoration: const BoxDecoration(
-                                        color: Colors.green,
-                                        shape: BoxShape.circle,
+                                      Positioned(
+                                        bottom: 0,
+                                        right: 0,
+                                        child: Container(
+                                          width: 10,
+                                          height: 10,
+                                          decoration: const BoxDecoration(
+                                            color: Color.fromARGB(
+                                              255,
+                                              0,
+                                              233,
+                                              8,
+                                            ),
+                                            shape: BoxShape.circle,
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                    ],
                                   ),
-                                ],
-                              ),
-                              const SizedBox(width: 10),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
+                                  const SizedBox(height: 8),
                                   Text(
                                     'Names',
                                     style: TextStyle(
                                       color: Colors.white,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    'Hey there',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 14,
+                                      fontSize: screenWidth * 0.03,
                                     ),
                                   ),
                                 ],
                               ),
-                              const Spacer(),
-                              Text(
-                                'dd/mm/yyyy',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 12,
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
+                    if (hasMessages) SizedBox(height: 20),
+                    hasMessages
+                        ? Column(
+                            children: List.generate(
+                              20,
+                              (index) => Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 8.0,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Stack(
+                                      children: [
+                                        Container(
+                                          width: 40,
+                                          height: 40,
+                                          decoration: const BoxDecoration(
+                                            color: Colors.white,
+                                            shape: BoxShape.circle,
+                                          ),
+                                        ),
+                                        Positioned(
+                                          bottom: 0,
+                                          right: 0,
+                                          child: Container(
+                                            width: 10,
+                                            height: 10,
+                                            decoration: const BoxDecoration(
+                                              color: Colors.green,
+                                              shape: BoxShape.circle,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'Names',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'Hey there',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const Spacer(),
+                                    Text(
+                                      'dd/mm/yyyy',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        : Container(
+                            height: screenHeight * 0.8,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.message,
+                                  color: Colors.white,
+                                  size: screenWidth * 0.2,
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  'Get in touch with any service provider to start a conversation here',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: screenWidth * 0.045,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ),
                   ],
                 ),
               ),
