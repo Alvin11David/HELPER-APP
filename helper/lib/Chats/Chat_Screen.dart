bca665034eac74934db9840de3f5df16f45eb13f
Alvin11David <alvin69david@gmail.com>
2026-01-18 13:57:30 +0300

    Working on the chat screen

---
+++ helper/lib/Chats/Chat_Screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background/normalscreenbg.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(child: Text('Chat Screen')),
+        ),
+      ),
+    );
+  }
+}
