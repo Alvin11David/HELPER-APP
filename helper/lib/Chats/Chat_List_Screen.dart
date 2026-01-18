00a3febffeda8d3d2305aa0f72f6231ab2d6d2a0
Alvin11David <alvin69david@gmail.com>
2026-01-18 11:26:10 +0300

    Working on the chat list screen

--- helper/lib/Chats/Chat_List_Screen.dart
+++ helper/lib/Chats/Chat_List_Screen.dart
@@ -5,61 +5,80 @@ class ChatListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/background/normalscreenbg.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Positioned(
-              top: screenWidth * 0.05,
-              left: screenWidth * 0.04,
-              child: Text(
-                'Messages',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: screenWidth * 0.06,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Positioned(
-              top: screenWidth * 0.05,
-              right: screenWidth * 0.04,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    width: screenWidth * 0.1,
-                    height: screenWidth * 0.1,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/background/normalscreenbg.png'),
+                      fit: BoxFit.cover,
                     ),
-                    child: const Icon(Icons.person, color: Colors.black),
                   ),
-                  SizedBox(width: screenWidth * 0.025),
-                  Container(
-                    width: screenWidth * 0.1,
-                    height: screenWidth * 0.1,
-                    decoration: const BoxDecoration(
+                ),
+                Positioned(
+                  top: screenWidth * 0.05,
+                  right: screenWidth * 0.04,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: screenWidth * 0.12,
+                        height: screenWidth * 0.12,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(Icons.person, color: Colors.black),
+                      ),
+                      SizedBox(width: screenWidth * 0.025),
+                      Container(
+                        width: screenWidth * 0.12,
+                        height: screenWidth * 0.12,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          Icons.notifications,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: screenWidth * 0.04,
+                  child: Text(
+                    'Messages',
+                    style: TextStyle(
                       color: Colors.white,
-                      shape: BoxShape.circle,
+                      fontSize: screenWidth * 0.06,
+                      fontWeight: FontWeight.bold,
                     ),
-                    child: const Icon(Icons.notifications, color: Colors.black),
                   ),
-                ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: screenWidth * 0.1,
+            left: screenWidth * 0.04,
+            child: Text(
+              'Messages',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: screenWidth * 0.06,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
