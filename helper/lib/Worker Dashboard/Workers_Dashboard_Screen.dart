7bb50605ea6a66a831c95bbc6c64038608bc5122
Alvin11David <alvin69david@gmail.com>
2026-01-19 13:28:15 +0300

    Working on the Workers Dashboard

---
+++ helper/lib/Worker Dashboard/Workers_Dashboard_Screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WorkersDashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background/normalscreenbg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Text('Worker Dashboard'), // Placeholder content
+        ),
+      ),
+    );
+  }
+}
