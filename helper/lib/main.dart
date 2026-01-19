89372769255455d9ae1d26508aa02c458b6dc761
Alvin11David <alvin69david@gmail.com>
2026-01-20 00:17:49 +0300

    Finished Edit

--- helper/lib/main.dart
+++ helper/lib/main.dart
@@ -66,7 +66,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:  ActiveJobScreen(),
+      home:  WorkerEarningsScreen(),
     );
   }
 }
