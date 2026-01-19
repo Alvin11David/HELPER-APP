83d3c529defa52b9512d1b45d89dedf64390a553
FidelJones <leftoversme@gmail.com>
2026-01-19 18:09:48 +0300

    Active job detail screen done

--- helper/lib/main.dart
+++ helper/lib/main.dart
@@ -11,6 +11,7 @@ import 'package:helper/Wallet/Wallet_TopUp_Screen.dart';
 import 'package:helper/Wallet/Wallet_Withdraw_Screen.dart';
 import 'package:helper/Worker%20Dashboard/Worker_Details_Screen.dart';
 import 'package:helper/Worker%20Dashboard/Workers_Dashboard_Screen.dart';
+import 'package:helper/Worker%20Dashboard/Active_Job_detail.dart';
 import 'package:helper/Worker%20Dashboard/Workers_skills_and_Job_Details.dart';
 import 'firebase_options.dart';
 import 'package:helper/Auth/OTP_Verification_Screen.dart';
@@ -59,7 +60,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const WalletFlowScreen(), // Or WorkersDashboardScreen() depending on your needs
+      home: const ActiveJobScreen(),
     );
   }
 }
