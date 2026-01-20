2881682e4de3e3c522c412610b5bf0b860e2a9ac
Alvin11David <alvin69david@gmail.com>
2026-01-20 18:15:21 +0300

    Working on the flutter wave

--- helper/lib/Payments/Registration_Payment_Screen.dart
+++ helper/lib/Payments/Registration_Payment_Screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'Mastercard_Payment_Method_Screen.dart';
 import 'Visa_Payment_Method_Screen.dart';
 import 'MTN_Payment_Method_Screen.dart';
+import 'Airtel_Payment_Method_Screen.dart';
 
 class RegistrationPaymentScreen extends StatefulWidget {
   const RegistrationPaymentScreen({super.key});
