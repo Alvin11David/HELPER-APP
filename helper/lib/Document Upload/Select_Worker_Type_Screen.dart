8350994e022f0fbed72d97da65656b6cf0d96d07
Alvin11David <alvin69david@gmail.com>
2026-01-21 14:04:27 +0300

    Added the Google

--- helper/lib/Document Upload/Select_Worker_Type_Screen.dart
+++ helper/lib/Document Upload/Select_Worker_Type_Screen.dart
@@ -320,42 +320,6 @@ class _SelectWorkerTypeScreenState
                         ),
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.05),
-                Center(
-                  child: SizedBox(
-                    width: screenWidth * 0.9,
-                    height: screenHeight * 0.07,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Continue',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: screenWidth * 0.045,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Poppins",
-                            ),
-                          ),
-                          SizedBox(width: screenWidth * 0.03),
-                          Icon(
-                            Icons.arrow_forward,
-                            color: Colors.black,
-                            size: screenWidth * 0.05,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
                   ],
                 ),
               ),
