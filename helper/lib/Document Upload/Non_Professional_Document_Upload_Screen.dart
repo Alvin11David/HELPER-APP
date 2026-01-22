f4acc2705f1a0d2a4b064b40507a6dda9f9c5e4d
Alvin11David <alvin69david@gmail.com>
2026-01-22 13:57:55 +0300

    Working on ID upload screens

--- helper/lib/Document Upload/Non_Professional_Document_Upload_Screen.dart
+++ helper/lib/Document Upload/Non_Professional_Document_Upload_Screen.dart
@@ -202,7 +202,7 @@ class _NonProfessionalDocumentUploadScreenState
                     SizedBox(height: h * 0.05),
                     Container(
                       width: 361,
-                      height: 138,
+                      height: 140,
                       padding: EdgeInsets.all(w * 0.04),
                       alignment: Alignment.topCenter,
                       decoration: BoxDecoration(
