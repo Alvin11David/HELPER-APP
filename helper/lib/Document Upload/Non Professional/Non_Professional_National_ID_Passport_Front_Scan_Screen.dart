57c975518705981842a2ab3be75798243a697ef5
Alvin11David <alvin69david@gmail.com>
2026-01-22 18:52:46 +0300

    Working on ID upload screens

--- helper/lib/Document Upload/Non Professional/Non_Professional_National_ID_Passport_Front_Scan_Screen.dart
+++ helper/lib/Document Upload/Non Professional/Non_Professional_National_ID_Passport_Front_Scan_Screen.dart
@@ -5,6 +5,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:helper/Document%20Upload/National_ID_Passport_Front_Upload_Screen.dart';
+import 'package:helper/Document%20Upload/Non%20Professional/Non_Professional_National_ID_Passport_Front_Upload_Screen.dart';
 
 class NonProfessionalNationalIdPassportFrontScanScreen extends StatefulWidget {
   final int selected; // 0 for National ID, 1 for Passport
@@ -400,7 +401,7 @@ class _NonProfessionalNationalIdPassportFrontScanScreenState
                                               await Navigator.of(context).push(
                                                 MaterialPageRoute(
                                                   builder: (context) =>
-                                                      NationalIdPassportFrontUploadScreen(
+                                                      NonProfessionalNationalIdPassportFrontUploadScreen(
                                                         selected:
                                                             widget.selected,
                                                         initialImage:
