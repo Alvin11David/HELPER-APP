f94c549c415b4807cd29ab83953790d63b605d5e
Alvin11David <alvin69david@gmail.com>
2026-01-24 15:41:22 +0300

    Working on Support screen

---
+++ helper/lib/Document Upload/Profile/Support_Screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SupportScreen extends StatelessWidget {
+  const SupportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background/normalscreenbg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const Center(
+          child: Text('Support Screen'), // Placeholder content
+        ),
+      ),
+    );
+  }
+}
