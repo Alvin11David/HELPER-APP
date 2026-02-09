c9c0b0a1a014da4ad4432a32580bf53fdd508300
Alvin11David <alvin69david@gmail.com>
2026-02-09 15:51:44 +0300

    Worked on the Airtel pay screen

--- helper/lib/Wallet/Wallet_Cancelled_screen.dart
+++ helper/lib/Wallet/Wallet_Cancelled_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helper/Chats/overlays/incoming_call_overlay_service.dart';
 import 'Wallet_Deposit_Payment_Method_Screen.dart';
+import 'Wallet_Withdraw_Payment_Method_Screen.dart';
 
 class WalletFlowScreen extends StatefulWidget {
   const WalletFlowScreen({super.key});
@@ -379,7 +380,18 @@ class _WalletFlowScreenState extends State<WalletFlowScreen> {
                                 MaterialPageRoute(
                                   builder: (context) =>
                                       const WalletDepositPaymentMethodScreen(
-                                          amount: "0"),
+                                        amount: "0",
+                                      ),
+                                ),
+                              );
+                            } else if (m == _ActionMode.withdraw) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const WalletWithdrawPaymentMethodScreen(
+                                        amount: "0",
+                                      ),
                                 ),
                               );
                             } else {
